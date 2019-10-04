29674a7d355bc1ed088a61c43f9a6401dac83d25
Yahaira Berries <yberries@il-e3.21-school.ru>
2019-10-04 19:37:58 +0300

    fucking select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("bootstrap/dist/js/bootstrap")
 require("icheck")
+require("select2.min")
 require("hider")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -17,9 +17,3 @@ require("hider")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-import 'bootstrap'
-
-document.addEventListener("turbolinks:load", () => {
-	$('[data-toggle="tooltip"]').tooltip()
-})
